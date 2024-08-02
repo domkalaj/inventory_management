51792f1cdc03cc1bd628dc38a189272e982e3e4e
dkalaj1239 <domkalaj1239@gmail.com>
2024-08-02 13:14:14 -0400

    add fncs (remove, add)

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { firestore } from "@/firebase";
 import { Box, Typography } from "@mui/material";
-import { collection, query, getDocs } from "firebase/firestore";
+import { collection, query, getDocs, doc } from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setInvetory] = useState([]);
@@ -11,9 +11,9 @@ export default function Home() {
   const [itemName, setItemName] = useState("");
 
   const updateInventory = async () => {
-    const snapshot = query(collection(firestore, "inventory"))
-    const docs = await getDocs(snapshot)
-    const inventoryList = []
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
     docs.forEach((doc) => {
       inventoryList.push({
         name: doc.id,
@@ -22,7 +22,34 @@ export default function Home() {
     });
   };
 
-  
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 });
+      }
+    }
+
+    await updateInventory();
+  };
+
+  const additem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      await setDoc(docRef, { quantity: quantity + 1 });
+    } else {
+      await setDoc(deocRef, { quantity: 1 });
+    }
+    await updateInventory();
+  };
 
   useEffect(() => {
     updateInventory();
@@ -32,7 +59,7 @@ export default function Home() {
     <Box>
       <Typography variant="h1">Inventory Management</Typography>
       {inventory.forEach((item) => {
-        console.log(item)
+        console.log(item);
         return (
           <>
             {item.name}
